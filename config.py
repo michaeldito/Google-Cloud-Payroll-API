c0c85ade818c7ce24c0995e3f82821fc9dafda03
Michael Dito <mcdito13@gmail.com>
2017-12-05 10:37:55 +0000

    long overdue intial commit

---
+++ config.py
@@ -0,0 +1,6 @@
+PROJECT_ID = 'cs-385-cloudpay'
+DATA_BACKEND = 'cloudsql'
+CLOUDSQL_USER = 'root'
+CLOUDSQL_PASSWORD = 'cs385'
+CLOUDSQL_DATABASE = 'timesheets'
+CLOUDSQL_CONNECTION_NAME = 'cs-385-cloudpay:us-west1:timesheets'
---
+++ start-server.sh
@@ -0,0 +1,1 @@
+sudo FLASK_APP=~/restserver/restserver.py flask run --host=0.0.0.0 --port=80
