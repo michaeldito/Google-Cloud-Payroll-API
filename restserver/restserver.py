151f018c745d59128f9e0a4ab12930a9dbdaa549
Michael Dito <mcdito13@gmail.com>
2017-12-06 06:28:23 +0000

    working demo - application-0 updates

--- restserver/restserver.py
+++ restserver/restserver.py
@@ -3,12 +3,32 @@ import json
 import datetime
 from google.cloud import pubsub_v1
 
+import logging
+logging.basicConfig()
+
 app = Flask(__name__)
 
 paystubs = {}
 
 subscriber = pubsub_v1.SubscriberClient()
 paystub_delivery_subscription_path = subscriber.subscription_path('cs-385-cloudpay', 'application-0-paystub-delivery')
+
+msg = ""
+def callback(message):
+	print('Received message {}'.format(message))
+	print('Ackknowledging...')
+	message.ack()
+	print('Printing with json.loads...')
+	data = json.loads(message.data)
+	print json.dumps(data, indent=2)
+	print('Adding to paystubs')
+	ssn = int(data['ssn'])
+	print('Checking ssn type')
+	print(type(ssn))
+	paystubs[ssn] = data
+	print(paystubs[ssn])
+
+subscriber.subscribe(paystub_delivery_subscription_path, callback=callback)
 
 @app.route("/submit", methods=['POST', 'GET'])
 def submit():
@@ -41,17 +61,17 @@ def create_company():
 
 @app.route("/addEmployee", methods=['POST', 'GET'])
 def add_employee():
-    batch = request.get_json(force=True)
-    print('--- Incoming Request ---')
-    print json.dumps(batch,indent=4)
-    print('---- End of Request ----')
+	batch = request.get_json(force=True)
+	print('--- Incoming Request ---')
+	print json.dumps(batch,indent=4)
+	print('---- End of Request ----')
 
-    publisher = pubsub_v1.PublisherClient()
-    topic_path = publisher.topic_path('cs-385-cloudpay', 'insert-new-employee')
-    data = request.data.encode('utf-8')
-    publisher.publish(topic_path, data=data)
+	publisher = pubsub_v1.PublisherClient()
+	topic_path = publisher.topic_path('cs-385-cloudpay', 'insert-new-employee')
+	data = request.data.encode('utf-8')
+	publisher.publish(topic_path, data=data)
 
-    return Response(response=json.dumps(batch, indent=2), status=200, mimetype="application/json")
+	return Response(response=json.dumps(batch, indent=2), status=200, mimetype="application/json")
 
 
 @app.route("/deliveryRequest", methods=['POST', 'GET'])
@@ -66,17 +86,6 @@ def delivery_request():
 	data = request.data.encode('utf-8')
 	publisher.publish(topic_path, data=data)
 
-	msg = ""
-	def callback(message):
-		print('Received message {}'.format(message))
-		message.ack()
-		data = json.loads(message.data)
-		print json.dumps(data, indent=2)
-		print('End of Message\n')
-		ssn = data['employee_ssn']
-		paystubs[ssn] = data
-
-	subscriber.subscribe(paystub_delivery_subscription_path, callback)
 	return Response(response=json.dumps(batch, indent=2), status=200, mimetype="application/json")
 
 @app.route("/paystub", methods=['POST', 'GET'])
@@ -86,8 +95,10 @@ def paystub():
 	print json.dumps(batch,indent=4)
 	print('---- End of Request ----')
 
-	data = json.loads(batch)
-	ssn = data['employee_ssn']
+	data = request.get_json(force=True)
+	print('Inside paystub\nPrinting request.get_json()')
+	print(data)
+	ssn = data['ssn']
 
 	paystub = paystubs[ssn]
 
@@ -95,17 +106,17 @@ def paystub():
 
 @app.route("/calculateAccruals", methods=['POST', 'GET'])
 def calculate_accruals():
-    batch = request.get_json(force=True)
-    print('--- Incoming Request ---')
-    print json.dumps(batch,indent=4)
-    print('---- End of Request ----')
+	batch = request.get_json(force=True)
+	print('--- Incoming Request ---')
+	print json.dumps(batch,indent=4)
+	print('---- End of Request ----')
 
-    publisher = pubsub_v1.PublisherClient()
-    topic_path = publisher.topic_path('cs-385-cloudpay', 'calculate-accruals')
-    data = request.data.encode('utf-8')
-    publisher.publish(topic_path, data=data)
+	publisher = pubsub_v1.PublisherClient()
+	topic_path = publisher.topic_path('cs-385-cloudpay', 'calculate-accruals')
+	data = request.data.encode('utf-8')
+	publisher.publish(topic_path, data=data)
 
-    return Response(response=json.dumps(batch, indent=2), status=200, mimetype="application/json")
+	return Response(response=json.dumps(batch, indent=2), status=200, mimetype="application/json")
 
 
 if __name__ == '__main__':
--- restserver/start-server.sh
+++ restserver/start-server.sh
@@ -1,1 +1,1 @@
-sudo FLASK_APP=~/restserver/restserver.py flask run --host=0.0.0.0 --port=80
+sudo FLASK_APP=~/cloudpay/restserver/restserver.py flask run --host=0.0.0.0 --port=80
